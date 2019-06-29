3f611237addead9f8733c31bd257f9288fd2457b
Shu-Kai Chang <cshukai@gmail.com>
2019-06-28 23:17:59 -0500

    Update orgFilesForLongRun.sh

--- orgFilesForLongRun.sh
+++ orgFilesForLongRun.sh
@@ -1,7 +1,9 @@
 count=1
-for file in $(ls -U | head -n 20000)
+for file in $(ls -U img|  grep -v / | head -n 20000)
 do
   temp='img/'
+  #echo $file
+  #echo $temp$count
   mv $file $temp$count
   count=`expr $count + 1`
 done
