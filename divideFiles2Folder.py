d41660a76d05ea504fb466ef5b8015d7afe9f76c
Shu-Kai Chang <cshukai@gmail.com>
2019-06-30 16:06:20 -0500

    Create divideFiles2Folder.py

---
+++ divideFiles2Folder.py
@@ -0,0 +1,33 @@
+import os
+import math
+import shutil
+import glob
+
+
+batch_size=20000
+l=os.listdir('/home/shchang/scratch/cvpr/data/img/')
+seg=int(math.ceil(len(l)/batch_size))
+
+for i in range(seg):
+    foldername='/home/shchang/scratch/cvpr/data/img/'+'img'+str(i)
+    print(foldername)
+    if os.path.exists(foldername)== False :
+        os.mkdir(foldername)
+   
+def getDividor(l,batch_size):
+    for i in range(1, len(l), batch_size):  
+        yield l[i:i + batch_size] 
+
+dividors=getDividor(l,batch_size)
+serialNum=0
+for i in dividors:
+    dest="/home/shchang/scratch/cvpr/data/img/img"+str(serialNum)+"/"
+    source="/home/shchang/scratch/cvpr/data/img/"
+    for idx,filename in enumerate(i):
+        print(filename)
+        src_path=source+filename
+        dest_path=dest+filename
+        print(dest_path)
+        shutil.move(src_path, dest_path)
+        print('------------')
+    serialNum=serialNum+1    
