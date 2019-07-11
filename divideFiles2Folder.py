182090eb60ecae277ef9e8f0b928013e88a68fdf
Shu-Kai Chang <cshukai@gmail.com>
2019-07-10 20:38:29 -0500

    Update divideFiles2Folder.py

--- divideFiles2Folder.py
+++ divideFiles2Folder.py
@@ -5,31 +5,43 @@ import glob
 
 
 batch_size=20000
-l=os.listdir('/home/shchang/scratch/cvpr/data/img/')
+l=sorted(os.listdir('/home/shchang/scratch/cvpr/data/img/'))
+a=sorted(os.listdir('/home/shchang/scratch/cvpr/data/ann/voc/'))
 seg=int(math.ceil(len(l)/batch_size))
 
 for i in range(seg):
-    foldername='/home/shchang/scratch/cvpr/data/img/'+'img'+str(i)
+    foldername='/home/shchang/scratch/cvpr/data/img'+str(i)
+    annName='/home/shchang/scratch/cvpr/data/ann/voc'+str(i)
     if os.path.exists(foldername)== False :
         os.mkdir(foldername)
-   
+        os.mkdir(annName)
 def getDividor(l,batch_size):
     for i in range(1, len(l), batch_size):  
         yield l[i:i + batch_size] 
 
 dividors=getDividor(l,batch_size)
+a_dividors=getDividor(a,batch_size)
 serialNum=0
 for i in dividors:
-    dest="/home/shchang/scratch/cvpr/data/img/img"+str(serialNum)+"/"
+    dest="/home/shchang/scratch/cvpr/data/img/"+str(serialNum)+"/"
     source="/home/shchang/scratch/cvpr/data/img/"
+    dest_a="/home/shchang/scratch/cvpr/data/ann/voc"+str(serialNum)+"/"
+    source_a="/home/shchang/scratch/cvpr/data/ann/voc/"
     for idx,filename in enumerate(i):
+        print("image file")
         print(filename)
+        print("ann file")
+        print(a_dividor[idx])
         src_path=source+filename
         dest_path=dest+filename
-        print(src_path)
-        print(dest_path)
+        a_src_path=source_a+filenamea_dividor[idx]
+        a_dest_path=dest_a+filenamea_dividor[idx]
+        #print(src_path)
+        #print(dest_path)
         #shutil.move(src_path, dest_path)
         shutil.copy(src_path, dest_path)
         os.remove(src_path)
+        shutil.copy(a_src_path,a_dest_path)
+        os.remove(a_src_path)
         print('------------')
-    serialNum=serialNum+1
+    serialNum=serialNum+1    
